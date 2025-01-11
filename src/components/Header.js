c3311294565bfe059646d5bad8e78a6864120ec6
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-01-11 17:15:59 +0100

    Splitting store

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
+import { authActions } from '../store/auth';
 import classes from './Header.module.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth.isAuthenticated); //false
+
+  const handleLogout = () => {
+    dispatch(authActions.logout());
+  }
+
   return (
     <header className={classes.header}>
-      <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      <h1>Redux Auth</h1> 
+      {auth &&
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      }
     </header>
   );
 };
